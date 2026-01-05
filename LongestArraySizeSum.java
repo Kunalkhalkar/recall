90eb9b2c157757db9a102d15bc13e3e010c35d73
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-01-05 21:24:17 +0530

    sum

---
+++ LongestArraySizeSum.java
@@ -0,0 +1,26 @@
+/*
+1. Longest Subarray With Sum ≤ K
+Description:
+Given an integer list nums and integer k, return the maximum length of a contiguous subarray whose sum is ≤ k.
+Example:
+Input: nums=[2,3,1,2,4], k=7
+Output: 3
+Explanation: [3,1,2] has sum 6 ≤ 7.
+*/
+import java.util.*;
+public class LongestSubArraySizeSum{
+	public static void main(String...x){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Array Element");
+		int a[] = new Int[5];
+		
+		for(int i =0; i<a.length; i)
+		{
+			a[i] = sc.nextInt();
+		}
+		
+		int i= 0, j=0;
+		while(j <= a.length)
+	}
+	
+}
