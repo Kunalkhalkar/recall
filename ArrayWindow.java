7e49d7b98f63e8052dc0b77d9087b0c88ae257cc
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-02-10 11:47:58 +0530

    kunnal

---
+++ ArrayWindow.java
@@ -0,0 +1,30 @@
+/*
+dynamic sliding window
+ Problem:Find the maximum sum of any subarray.
+Example:
+Input: [-2, 1,  -3,   4,   -1,  2,  1,   -5.  ,4]  
+Output: 6.  (from subarray [4,-1,2,1])
+*/
+import java.util.*;
+public class ArrayWindow{
+	public static void main(String ...x){
+		Scanner sc = new Scanner(System.in);
+		int a[] = new int[9];
+		System.out.println("Enter the element in array");
+		
+		for(int i=0; i<a.length; i++){
+			a[i] = sc.nextInt();	
+		}
+		
+		int i=0, j=1;
+		
+		if(a.length < 1){
+			System.out.println("array length is zero");
+			System.exit();
+		}
+		while(j < a.length){
+			
+		}
+		
+	}
+}
