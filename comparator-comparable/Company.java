37452097a218009d53f66c19b517c73bc4b6ce08
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-03-03 20:20:39 +0530

    Initial commit

---
+++ comparator-comparable/Company.java
@@ -0,0 +1,12 @@
+/*
+Sorting Employees by salary
+
+Sorting Map by value
+
+Sorting custom objects with 3 conditions
+*/
+class Employee implements comparable<Emplpoyee>{
+}
+public class Company{
+
+}
