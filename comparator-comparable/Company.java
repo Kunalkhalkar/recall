dc7a35c62c96792cf119530d6360171c9507de93
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-03-03 21:04:55 +0530

    commit

--- comparator-comparable/Company.java
+++ comparator-comparable/Company.java
@@ -5,8 +5,16 @@ Sorting Map by value
 
 Sorting custom objects with 3 conditions
 */
-class Employee implements comparable<Emplpoyee>{
+class Employee implements Comparable<Emplpoyee>{
+	
 }
 public class Company{
-
+	public static void main(String ...x){
+		System.out.println("Enter the id, name, salary");
+		Scanner sc = new Scanner(System.in);
+		int id = sc.nextInt();
+		String name = sc.next();
+		int salary = sc.nextInt();
+		
+	}
 }
