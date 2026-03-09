1f7902e4796125f8a53a30a19c6c1375a7bc9477
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-03-09 19:38:57 +0530

    q3

--- L-2_exam/Q3.java
+++ L-2_exam/Q3.java
@@ -10,3 +10,9 @@ Input: nums = [1,0,-1,0,-2,2], target = 0
 Output: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
 
 */
+import java.util.*;
+public class Q3{
+	public static void main(String ...x){
+		
+	}
+}
