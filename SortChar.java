8369384a94f16913e39cf349669690e57f54c2f2
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-04-17 23:44:00 +0530

    kunal

--- SortChar.java
+++ SortChar.java
@@ -1,3 +1,18 @@
 /*
 451. Sort Characters By Frequency(Medium)
 */
+import java.util.*;
+public class SortChar{
+	public static void main(String ...x){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the String");
+		String s = sc.next();
+		System.out.println(frequencySort(s));
+	}
+	
+	public static String frequencySort(String s){
+	
+	Map<Character, Integer> map = new HashMap<>();
+
+	}
+}
