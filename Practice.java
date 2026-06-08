61c0ef5c7b7bcc5ddd3e8652341950f1dad7a1bd
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-06-08 21:15:51 +0530

    practice

--- Practice.java
+++ Practice.java
@@ -2,11 +2,14 @@ import java.util.*;
 public class Practice{
 	public static void main(String ...x){
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the data you want to store");
-		String data = sc.nextLine();
-		DataStore ds = new DataStore(data);
-		System.out.println(ds.getData());
-		
+		int i=3;
+		while(i >= 0){
+			System.out.println("Enter the data you want to store");
+			String data = sc.nextLine();
+			DataStore ds = new DataStore(data);
+			System.out.println(ds.getData()+" "+ i);
+			i--;
+		}
 	}
 }
 public class DataStore{
