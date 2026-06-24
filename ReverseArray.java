605309ef7d7d05dcd2026eb247b33fc9449be077
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-06-24 19:49:35 +0530

    Initial commit

--- ReverseArray.java
+++ ReverseArray.java
@@ -8,7 +8,7 @@ import java.util.*;
 public class ReverseArray{
 	public static void main(String...x) throws Exception{
 		System.out.println("Enter the array elements");
-		int arr [] = new int[5];
+		int arr [] = new int[6];
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		for(int i=0; i < arr.length; i++){
 			arr[i]  = Integer.parseInt(br.readLine());
@@ -16,10 +16,10 @@ public class ReverseArray{
 		System.out.println("Array without reverse");	
 		System.out.println(Arrays.toString(arr));
 		System.out.println("reversed Array using two pointer approch");
-		System.out.println(Arrays.toStringn(reverseArray(arr)));
+		System.out.println(Arrays.toString(reverseArray(arr)));
 	}
 	
-	public int[] reverseArray(int ...arr){
+	public static int[] reverseArray(int ...arr){
 		int i=0, j=arr.length-1, temp;
 		while(i < j){
 			temp = arr[i];
@@ -28,7 +28,7 @@ public class ReverseArray{
 			i++;
 			j--;
 		}
-		
+		return arr;
 	}
 	
 	
