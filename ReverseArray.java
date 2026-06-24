b8a3d6da5588d86f010d4c63c740e50d0a710351
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-06-24 19:47:59 +0530

    Initial commit

--- ReverseArray.java
+++ ReverseArray.java
@@ -16,6 +16,20 @@ public class ReverseArray{
 		System.out.println("Array without reverse");	
 		System.out.println(Arrays.toString(arr));
 		System.out.println("reversed Array using two pointer approch");
+		System.out.println(Arrays.toStringn(reverseArray(arr)));
 	}
+	
+	public int[] reverseArray(int ...arr){
+		int i=0, j=arr.length-1, temp;
+		while(i < j){
+			temp = arr[i];
+			arr[i] = arr[j];
+			arr[j] = temp;
+			i++;
+			j--;
+		}
+		
+	}
+	
 	
 }
