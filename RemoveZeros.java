5e86b5418201ab8e397a72c5b839a7b1263bc118
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-07-06 19:55:56 +0530

    remmove zeros

---
+++ RemoveZeros.java
@@ -0,0 +1,44 @@
+/*
+remove zeros from array
+*/
+import java.util.*;
+
+public class RemoveZeros{
+	public static void main(String ...x){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the element in array");
+		int a[] = new int[5];
+		
+		for(int i =0; i<a.length; i++){
+			a[i] = sc.nextInt();
+		}
+		
+		// System.out.println(Arrays.toString(a));
+		removeZeros(a);
+	}
+	public static void removeZeros(int[] a){
+		if(a.length ==0) System.out.println("Array is empty");
+		int i= 0, j=0;
+		
+		while(j < a.length){
+			if(a[i] == 0){
+				if(a[j] != 0){
+					int temp = a[i];
+					a[i] = a[j];
+					a[j] = temp;
+					i++;
+				}
+				j++;
+			}
+			else{
+				i++;
+				j++;
+			}
+		}
+		// System.out.println(i);
+		for(int k =0; k< i; k++){
+			System.out.print(a[k]+ " ");
+		}
+		// System.out.println(Arrays.toString(a));
+	}
+}
