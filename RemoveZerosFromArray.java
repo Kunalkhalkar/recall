f673ccae91e293724cd9cc4204c779928540d948
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-07-11 18:05:35 -0930

    remove zeros

--- RemoveZerosFromArray.java
+++ RemoveZerosFromArray.java
@@ -17,3 +17,22 @@ public class RemoveZerosFromArray{
 		System.out.println(Arrays.toString(a));
 		removeZeros(a); //function calling 
 	}
+	public static void removeZeros(int[] a){
+		if(a.length ==0) System.out.println("Array is empty");
+		int i= 0, j=0;
+		//this loop checks the zeros and send it to the end
+		while(j < a.length){
+			if(a[i] == 0){
+				if(a[j] != 0){
+					int temp = a[i];
+					a[i] = a[j];
+					a[j] = temp;
+					i++;
+				}
+				j++;
+			}
+			else{
+				i++;
+				j++;
+			}
+		}
