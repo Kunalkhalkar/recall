784f27c3e581ffd3b62b9678259dc3917c9e41ba
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-07-12 16:32:37 +0530

    max sum

---
+++ MaxSumSubArray.java
@@ -0,0 +1,20 @@
+/*
+find maximum sum sub array
+*/
+import java.util.*;
+public class MaxSumSubArray{
+	public static void main(String...x){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the array element");
+		int a[] = new int[5];
+		
+		for(int i=0; i<a.length; i++){
+			a[i] = sc.nextInt();
+		}
+		
+		int i=0, j=0;
+		while(j < a.length){
+			
+		}
+	}
+}
