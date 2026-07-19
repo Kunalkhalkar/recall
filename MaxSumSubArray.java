12ab8483d44942f3a932d2a4adcf3eafb759922c
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-07-19 11:11:31 +0530

    max sum array

--- MaxSumSubArray.java
+++ MaxSumSubArray.java
@@ -12,9 +12,13 @@ public class MaxSumSubArray{
 			a[i] = sc.nextInt();
 		}
 		
-		int i=0, j=0;
+		int i=0, j=0, temp1, temp2, maxSum = -1;
 		while(j < a.length){
+			temp1 = a[i];
+			temp2 = a[j];
+			if(temp1 + temp2 > maxSum){
 			
+			}
 		}
 	}
 }
