c36815a90bef35fa506cc649ee002c369e64ce5d
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-08-07 20:21:54 +0530

    initial commit

---
+++ RemoveDuplicate.java
@@ -0,0 +1,19 @@
+/*
+remove duplicates from the array
+*/
+import java.util.*;
+import java.io.*;
+public class RemoveDuplicate{
+	public static void main(String ...x) throws Exception{
+		Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter the array elements");
+		
+		int arr[] = new int[5];
+		
+		for(int i = 0; i< arr.length; i++){
+			arr[i] = sc.nextInt();
+		}
+		
+	}
+}
