470132af7274460729c614b5aa8afcb4d8380a0f
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-08-20 21:26:28 +0530

    new update

--- SecMax.java
+++ SecMax.java
@@ -8,9 +8,7 @@ public class SecMax{
 		Scanner sc = new Scanner(System.in);
 		
 		for(int i =0; i<arr.length; i++){
-			
 			arr[i] = sc.nextInt();
-			
 		}
 		
 		int max = Integer.MIN_VALUE, secMax = Integer.MIN_VALUE;
