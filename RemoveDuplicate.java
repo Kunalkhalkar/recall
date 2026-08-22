c7856162a8f799b37ce663092ef2906852646bc4
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-08-22 19:08:15 +0530

    new Changes

--- RemoveDuplicate.java
+++ RemoveDuplicate.java
@@ -8,7 +8,6 @@ public class RemoveDuplicate{
 		Scanner sc = new Scanner(System.in);
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("Enter the array elements");
 
 		int arr[] = new int[5];		
