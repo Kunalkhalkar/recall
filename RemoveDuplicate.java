dd4900901ce56bb636ab7a7bbc71d18eaef1904b
kunal khalkar <131854920+Kunalkhalkar@users.noreply.github.com>
2026-08-26 19:10:22 +0530

    new

--- RemoveDuplicate.java
+++ RemoveDuplicate.java
@@ -3,6 +3,7 @@ remove duplicates from the array
 */
 import java.util.*;
 import java.io.*;
+
 public class RemoveDuplicate{
 	public static void main(String ...x) throws Exception{
 		Scanner sc = new Scanner(System.in);
@@ -10,13 +11,13 @@ public class RemoveDuplicate{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		System.out.println("Enter the array elements");
-
 		int arr[] = new int[5];		
 		for(int i = 0; i< arr.length; i++){
 			arr[i] = sc.nextInt();
 		}
 			
 		//if(arr.length < 2) return;
+			
 		for(int i = 0, j = arr.length - 1, k = 1; i < arr.length; ){
 			if(arr[i] == arr[k]){
 				i++;
